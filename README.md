6962d5e9204b6967840023001794d5f0610e3610
RDShah <Lntshah@yahoo.com>
2024-07-13 22:59:32 -0800

    comments and a readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# No Thanks Simulator and Bots
+
+Rules for the game "No Thanks!" can be found here: (https://en.wikipedia.org/wiki/No_Thanks!_(game))[https://en.wikipedia.org/wiki/No_Thanks!_(game)].
+
+`main.cpp` implements a simulator of the game with two players.
+`strats.cpp` has several bots (which you should add to!).
+`nothanks.h` has the `gamestate_t` struct and it's description.
--- nothanks.h
+++ nothanks.h
@@ -8,11 +8,29 @@
 #include <ranges>
 #include <tuple>
 
+/**
+ * a hand (a set of cards) is represented as a 64-bit integer
+ * bit j is set iff card #j is in the hand
+ * eg:
+ * hand_has_card_10 = (hand >> 10) & 1;
+ */
 typedef uint64_t u64;
 
 struct gamestate_t {
+    
+    /**
+     * cards[0] = hand of player 0
+     * cards[1] = hand of player 1
+     * cards[2] = set of cards still in deck
+     */
     u64 cards[3];
-    uint8_t pennies[4];
+    
+    /**
+     * pennies[0] = number of pennies held by player 0
+     * pennies[1] = number of pennies held by player 1
+     * pennies[2] = number of pennies in the middle
+     */
+    uint8_t pennies[3];
 };
 
 typedef bool Strategy(gamestate_t, int, int);
