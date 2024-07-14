c592e837770c4015f0ad921fa76063cd033e98bb
RDShah <Lntshah@yahoo.com>
2024-07-13 23:33:25 -0800

    adding a strat

--- strats.cpp
+++ strats.cpp
@@ -15,6 +15,14 @@ bool ratio_3_strat(gamestate_t gs, int player)
     return gs.offer > 3 * gs.pennies[2];
 }
 
+bool ratio_11_to_1_strat(gamestate_t gs, int player)
+{
+    int cardsleft = std::popcount(gs.cards[2]);
+    float frac = (float)(cardsleft - 9.0) / (33.0 - 9.0);
+    float pennyvalue = 1 + 10 * frac;
+    return gs.offer > pennyvalue * gs.pennies[2];
+}
+
 bool ratio_21_to_1_strat(gamestate_t gs, int player)
 {
     int cardsleft = std::popcount(gs.cards[2]);
@@ -26,8 +34,10 @@ bool ratio_21_to_1_strat(gamestate_t gs, int player)
 #define named_strat(strat) std::make_tuple(#strat, strat)
 auto strats = {
     // named_strat(always_no_thanks),
-    named_strat(always_takes_card),
+    // named_strat(always_takes_card),
     // named_strat(ratio_2_strat),
-    // named_strat(ratio_3_strat),
-    // named_strat(ratio_21_to_1_strat)
-    named_strat(high_cards_are_scary)};
+    named_strat(ratio_3_strat),
+    named_strat(ratio_21_to_1_strat),
+    named_strat(ratio_11_to_1_strat),
+    // named_strat(high_cards_are_scary)
+};
