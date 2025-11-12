65df0aa44a94f5b4404c5de92c626b5dc7e17cb1
RDShah <Lntshah@yahoo.com>
2025-11-12 14:41:17 -0800

    greedy bot

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from solver3 import prob_p1_win_row
+from game import *
 
 def bot(p, hand1, hand2, offer, pile):
     x = str((hand1 | hand2).bit_count() + 2)
@@ -7,3 +8,9 @@ def bot(p, hand1, hand2, offer, pile):
     value_of_taking = prob_p1_win_row(hand1 | (1 << offer), hand2)[p + pile]
     value_of_refusal = = 1 - prob_p1_win_row(hand2 | (1 << offer), hand1)[22 - p]
     return YES_PLEASE if value_of_taking > value_of_refusal else NO_THANKS
+
+def greedyfactory(level):
+    def greedybot(p, hand1, hand2, offer, pile):
+        return NO_THANKS if offer > pile * level else YES_PLEASE
+
+    return greedybot
